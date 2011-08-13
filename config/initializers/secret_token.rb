caba1370b2983af82a56cc809d5969e8fb901164
Fernando Kosh <fernando.kosh@gmail.com>
2011-08-12 21:24:58 -0300

    Commit inicial com aplicação Rails 3.1.0.rc5 base.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dumuzzi::Application.config.secret_token = 'e9f695328af3a868a746a5e9e82f3801ab37016545bdb59b3848f84b312f211b59e22136234f5e2139740dffb53baa8cc0bf8fcbca44cbc9748c7981911e4b2e'
