5afc37ebb81b38d8c0c2e111e72162276600d149
Vagner Zampieri <vgzampieri@gmail.com>
2011-08-14 20:42:13 -0300

    Hosts raly11

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @hosts = Host.where(:status => 1, :enabled => 1).order("name ASC")
+    @new_hosts = Host.where(:enabled => 1).order("created_at DESC")
   end
 end
