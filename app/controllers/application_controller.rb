173da86aa30fac1b3851c675aa31463b62fd8180
Vagner Zampieri <vgzampieri@gmail.com>
2011-08-14 18:30:19 -0300

    Validation rally11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :init
+  before_filter :init, :authorize
   
   def init
     @authentications = current_user.authentications if current_user
   end
+  
+  def authorize
+    controllers = %w(domains hosts hosts_services protocols queueds services state_changes)
+    unless user_signed_in?
+      controllers.each do |controller|    
+        if params[:controller] == controller
+          redirect_to "/"
+        end
+      end
+    end
+  end
+  
 end
