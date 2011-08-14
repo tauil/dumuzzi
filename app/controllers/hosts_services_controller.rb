1f65b16378925be0c99a2b7191c4350b6a7241d9
Igor Lanes <igorlanes@gmail.com>
2011-08-14 15:47:37 -0300

    Implementation of hosts_services. rally11

--- app/controllers/hosts_services_controller.rb
+++ app/controllers/hosts_services_controller.rb
@@ -17,6 +17,7 @@ class HostsServicesController < ApplicationController
   def new
     @hosts_service = HostsService.new :host_id => params[:host_id]
     @hosts_service.user_id = @hosts_service.host.user_id unless @hosts_service.host.nil?
+    @service = Service.all
     
     respond_with @hosts_service
   end
