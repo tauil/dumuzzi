808d3adf9a2180c893c748c0ab7609e45de221c7
Fernando Kosh <fernando.kosh@gmail.com>
2011-10-09 23:31:04 -0300

    Improving home hosts list finders.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @hosts = Host.where(:status => 1, :enabled => 1).order("name ASC")
-    @new_hosts = Host.where(:enabled => 1).order("created_at DESC")
+    @hosts = Host.where(:status_id => 1, :enabled => 1, :share => true).where("created_at < '#{(Time.now - 7.days)}'").order("name ASC")
+    @new_hosts = Host.where(:enabled => 1, :share => true).where("created_at > '#{DateTime.now - 7.days}'").where("id NOT IN ('#{@hosts.map(&:id).join(',')}')").order("created_at DESC")
   end
 end
