ebba38d6eefa856d6b814351830608438b547f76
Fernando Kosh <fernando.kosh@gmail.com>
2011-10-10 16:14:40 -0300

    Rename state_changes model to status_changes.

--- app/models/status_change.rb
+++ app/models/status_change.rb
@@ -1,5 +1,6 @@
-class StateChange < ActiveRecord::Base
+class StatusChange < ActiveRecord::Base
   before_create :generate_ids
+  belongs_to :host
  
   def generate_ids
     self.id = Digest::SHA1.hexdigest("#{Socket.gethostname} #{srand.to_s} #{DateTime.now.to_s}")
