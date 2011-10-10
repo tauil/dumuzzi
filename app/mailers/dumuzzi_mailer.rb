320c6ee54b1d4936c2120024678478b04e9dab49
Fernando Kosh <fernando.kosh@gmail.com>
2011-10-09 23:39:51 -0300

    Send mail for events test.

--- app/mailers/dumuzzi_mailer.rb
+++ app/mailers/dumuzzi_mailer.rb
@@ -1,14 +1,21 @@
+require 'rubygems'
+require 'action_mailer'
 class DumuzziMailer < ActionMailer::Base
-  default from: "from@example.com"
+    default from: "monitor@dumuzzi.com"
+
+ActionMailer::Base.config.template_root = "#{Dir.pwd}/app/views/dumuzzi_mailer/"
+ActionMailer::Base.config.delivery_method = :sendmail
+ActionMailer::Base.config.logger = Logger.new(STDOUT)
 
     def warning_message(warning)
       @warning = warning
-#      @host = ActionMailer::Base.default_url_options[:host]
+      @host = ActionMailer::Base.smtp_settings[:default_url_options]
       mail(
         :to => ActionMailer::Base.smtp_settings[:to],
         :subject => "Host alert!",
 	      :message => "Host down!"
 	    )
+	    puts "[Mailer] Mail sent."
 	  end
 
 end
