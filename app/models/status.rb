683791ab8a9f6d670c7fcdf791dc9336c25dd65f
Fernando Kosh <fernando.kosh@gmail.com>
2011-10-13 16:53:41 -0300

    Model relations.

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,4 +2,5 @@ class Status < ActiveRecord::Base
   has_many :domains
   has_many :hosts
   has_many :hosts_services
+  has_many :queueds
 end
--- app/models/status_change.rb
+++ app/models/status_change.rb
@@ -1,6 +1,13 @@
 class StatusChange < ActiveRecord::Base
   before_create :generate_ids
   belongs_to :host
+  belongs_to :host_service
+#  belongs_to :tester
+  belongs_to :service
+  belongs_to :interval
+#  belongs_to :from_status
+#  belongs_to :to_status
+  belongs_to :status
  
   def generate_ids
     self.id = Digest::SHA1.hexdigest("#{Socket.gethostname} #{srand.to_s} #{DateTime.now.to_s}")
