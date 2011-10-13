7614c21561a535ff5e47eda619b8fc2554ea1e07
Fernando Kosh <fernando.kosh@gmail.com>
2011-10-13 16:51:40 -0300

    Model relations and status_changed? added to hosts_service.

--- app/models/hosts_service.rb
+++ app/models/hosts_service.rb
@@ -3,11 +3,26 @@ class HostsService < ActiveRecord::Base
   belongs_to :host
   belongs_to :service
   belongs_to :status
+  has_many :status_changes
   belongs_to :interval
   has_many :queueds
-  
+
   def generate_ids
     self.id = Digest::SHA1.hexdigest("#{Socket.gethostname} #{srand.to_s} #{DateTime.now.to_s}")
     self.user_id = self.host.user_id
   end
+
+  def status_changed?
+    last_status = StatusChange.where(:hosts_service_id => self.id).limit(1).order('created_at DESC')
+    unless last_status.empty?
+      unless last_status.status_id == self.status_id
+        true
+      else
+        false
+      end
+    else
+      true
+    end
+  end
+
 end
