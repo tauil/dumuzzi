cda4b4b2c9d69e5d4c44e4910696e1189368ff09
Fernando Kosh <fernando.kosh@gmail.com>
2011-10-14 00:26:38 -0300

    I dontknow why i've created this migration.

--- db/migrate/20111010235430_add_host_services_fields.rb
+++
@@ -1,6 +0,0 @@
-class AddHostServicesFields < ActiveRecord::Migration
-  def change
-    add_column :hosts_services, :host_service_id, :integer
-    add_index :hosts_services, :host_service_id
-  end
-end
