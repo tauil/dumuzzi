98016003626a9efce57f9eac7b4eba8751fcfd5c
Fernando Kosh <fernando.kosh@gmail.com>
2011-10-14 03:51:45 -0300

    StatusChange belongs_to :tester, :from_status and :to_status.

--- app/models/status_change.rb
+++ app/models/status_change.rb
@@ -2,11 +2,11 @@ class StatusChange < ActiveRecord::Base
   before_create :generate_ids
   belongs_to :host
   belongs_to :hosts_service
-#  belongs_to :tester
+  belongs_to :tester, :class_name => 'Host'
   belongs_to :service
   belongs_to :interval
-#  belongs_to :from_status
-#  belongs_to :to_status
+  belongs_to :from_status, :class_name => 'Status'
+  belongs_to :to_status, :class_name => 'Status'
   belongs_to :status
  
   def generate_ids
