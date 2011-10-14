38298e09818f8658e5754307fd5a2dbb37eda1b1
Fernando Kosh <fernando.kosh@gmail.com>
2011-10-14 03:53:06 -0300

    Removed unnecessary require files.

--- app/mailers/dumuzzi_mailer.rb
+++ app/mailers/dumuzzi_mailer.rb
@@ -1,21 +1,15 @@
-require 'rubygems'
-require 'action_mailer'
 class DumuzziMailer < ActionMailer::Base
-    default from: "monitor@dumuzzi.com"
+  default from: "monitor@dumuzzi.com"
 
-ActionMailer::Base.config.template_root = "#{Dir.pwd}/app/views/dumuzzi_mailer/"
-ActionMailer::Base.config.delivery_method = :sendmail
-ActionMailer::Base.config.logger = Logger.new(STDOUT)
-
-    def warning_message(warning)
-      @warning = warning
-      @host = ActionMailer::Base.smtp_settings[:default_url_options]
-      mail(
-        :to => ActionMailer::Base.smtp_settings[:to],
-        :subject => "Host alert!",
-	      :message => "Host down!"
-	    )
-	    puts "[Mailer] Mail sent."
-	  end
+  def warning_message(warning)
+    @warning = warning
+    @host = ActionMailer::Base.smtp_settings[:default_url_options]
+    mail(
+      :to => ActionMailer::Base.smtp_settings[:to],
+      :subject => "Host alert!",
+      :message => "Host down!"
+	  )
+	  puts "[Mailer] Mail sent."
+  end
 
 end
