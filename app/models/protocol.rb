06f13132e93d76c549605bf464481a994665d7eb
Fernando Kosh <fernando.kosh@gmail.com>
2011-10-14 03:52:16 -0300

    Fix has_many mane.

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,6 +1,6 @@
 class Protocol < ActiveRecord::Base
   before_create :generate_ids
-  has_many :service
+  has_many :services
  
   def generate_ids
     self.id = Digest::SHA1.hexdigest("#{Socket.gethostname} #{srand.to_s} #{DateTime.now.to_s}")
