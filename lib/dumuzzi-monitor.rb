3541f3b145948a3f5a7e66ae1becd6419b73147d
Fernando Kosh <fernando.kosh@gmail.com>
2011-10-15 18:05:46 -0300

    Readding required files.

--- lib/dumuzzi-monitor.rb
+++ lib/dumuzzi-monitor.rb
@@ -1,12 +1,18 @@
-
+require "#{Dir.pwd}/app/models/host"
+require "#{Dir.pwd}/app/models/user"
+require "#{Dir.pwd}/app/models/domain"
+require "#{Dir.pwd}/app/models/status"
+require "#{Dir.pwd}/app/models/hosts_service"
+require "#{Dir.pwd}/app/models/service"
+require "#{Dir.pwd}/app/models/queued"
+require "#{Dir.pwd}/app/models/status_change"
 require "#{Dir.pwd}/lib/dumuzzi-monitor/collector"
 require "#{Dir.pwd}/lib/dumuzzi-monitor/tester"
 
 module DumuzziMonitor
   extend self
   def has_connection?(cached=false)
-    domain = Domain.where(:name => 'localdomain')[0]
-    tester = Host.where(:name => 'localhost', :domain_id => domain.id)[0]
+    tester = Host.where(:id => Rails.application.config.local_tester[:id])[0]
     unless cached
       unless Service.gateway_test_ping(tester)
         puts "[Network] Gateway error. Tests are disabled."
