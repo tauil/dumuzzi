9dc29a8b28ec946080601e7a14064ba6f787f1e1
Vagner Zampieri <vgzampieri@gmail.com>
2011-10-24 17:40:57 -0200

    Modificações em User

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.create(:user_id => Digest::SHA1.hexdigest('monitor'), :email => 'monitor@dumuzzi.com', :password => 'monitor' )
+  end
+
   test "the truth" do
     assert true
+  end
+  
+  test 'should be valid' do
+    assert @user, "User shouldn't be created"
   end
   
 end
