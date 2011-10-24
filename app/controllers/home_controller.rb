315b151765df400fdf413558214dfc462b04a0c6
Fernando Kosh <fernando.kosh@gmail.com>
2011-10-24 20:48:00 -0200

    Limit 5 nas lsitas de hosts da home.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @hosts = Host.where(:status_id => 1, :enabled => 1, :share => true, :tester => false).where("created_at < '#{(Time.now - 7.days)}'").order("created_at DESC")
-    @new_hosts = Host.where(:enabled => 1, :share => true, :tester => false).where("created_at > '#{DateTime.now - 7.days}'").where("id NOT IN ('#{@hosts.map(&:id).join(',')}')").order("created_at DESC")
+    @hosts = Host.where(:status_id => 1, :enabled => 1, :share => true, :tester => false).where("created_at < '#{(Time.now - 7.days)}'").order("created_at DESC").limit(5)
+    @new_hosts = Host.where(:enabled => 1, :share => true, :tester => false).where("created_at > '#{DateTime.now - 7.days}'").where("id NOT IN ('#{@hosts.map(&:id).join(',')}')").order("created_at DESC").limit(5)
     @testers = Host.where(:enabled => 1, :tester => true).order("created_at DESC")
   end
 end
