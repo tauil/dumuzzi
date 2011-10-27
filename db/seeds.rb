71bf54b96b048b23bb2f099a1fd3ec4ea832da6f
Fernando Kosh <fernando.kosh@gmail.com>
2011-10-27 13:19:49 -0200

    New service in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ ping = Service.create(
   :port => 7,
   :description => "Ping-Pong Echo.",
   :plugin=> "internal_ping",
+  :public => true,
   :monitor => true,
   :enabled => true
 )
@@ -17,6 +18,18 @@ http = Service.create(
   :port => 80,
   :description => "WWW World Wide Web HTTP",
   :plugin=> "internal_http",
+  :public => true,
+  :monitor => true,
+  :enabled => true
+)
+
+Service.create(
+  :name => "Host Activate",
+  :protocol => Protocol.find_by_name('TCP'),
+  :port => 7,
+  :description => "Host activation test service. Will test and activate the host test.",
+  :plugin=> "host_activate",
+  :public => false,
   :monitor => true,
   :enabled => true
 )
