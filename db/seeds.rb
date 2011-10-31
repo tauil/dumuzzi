d1ee4c4e724bbf9ae49f808512bfffcf06158d64
Fernando Kosh <fernando.kosh@gmail.com>
2011-10-31 17:56:52 -0200

    Fix in db/seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,16 +23,18 @@ http = Service.create(
   :enabled => true
 )
 
-Service.create(
-  :name => "Host Activate",
-  :protocol => Protocol.find_by_name('TCP'),
-  :port => 7,
-  :description => "Host activation test service. Will test and activate the host test.",
-  :plugin=> "host_activate",
-  :public => false,
-  :monitor => true,
-  :enabled => true
-)
+unless Service.find_by_plugin("host_activate")
+  Service.create(
+    :name => "Host Activate",
+    :protocol => Protocol.find_by_name('TCP'),
+    :port => 7,
+    :description => "Host activation test service. Will test and activate the host test.",
+    :plugin => "host_activate",
+    :public => false,
+    :monitor => true,
+    :enabled => true
+  )
+end
 
 interval = Interval.find(3)
 status = Status.find('-2')
